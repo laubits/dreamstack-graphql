a79f0e2bba1e3ed89b1c4dafa9a9f0ede40953f6
gaboesquivel <contact@gaboesquivel.com>
2020-02-08 13:28:42 -0600

    wip(rxdemux): chronicle streams

--- compose/services/rxdemux/src/chronicle.ts
+++ compose/services/rxdemux/src/chronicle.ts
@@ -1,15 +1,27 @@
-// import { Observable } from 'rxjs'
 import ConsumerServer from 'chronicle-consumer'
+import { Observable, Subscriber } from 'rxjs'
 
-const chronicleOptions = { host: 'api.telos.eostribe.io/streaming', port: 443, ackEvery: 10, async: true }
+const { CHRONICLE_HOST, CHRONICLE_PORT, CHRONICLE_ACK, CHRONICLE_ASYNC } = process.env
+
+if (!CHRONICLE_HOST) throw new Error('Missing CHRONICLE_HOST env var')
+if (!CHRONICLE_PORT) throw new Error('Missing CHRONICLE_PORT env var')
+if (!CHRONICLE_ACK) throw new Error('Missing CHRONICLE_ACK env var')
+if (!CHRONICLE_ASYNC) throw new Error('Missing CHRONICLE_ASYNC env var')
+
+const chronicleOptions = { host: CHRONICLE_HOST, port: CHRONICLE_PORT, ackEvery: CHRONICLE_ACK, async: CHRONICLE_ASYNC }
 const chronicleServer = new ConsumerServer(chronicleOptions)
 
-chronicleServer.on('fork', (data: {}) => console.log(data))
-chronicleServer.on('tx', (data: {}) => console.log(data))
-chronicleServer.on('fork', (bnum: number) => console.log(bnum))
+export const fork$ = new Observable<{}>((subscriber: Subscriber<{}>) => {
+  chronicleServer.on('fork', (data: {}) => subscriber.next(data))
+})
+
+export const tx$ = new Observable<{}>((subscriber: Subscriber<{}>) => {
+  chronicleServer.on('tx', (data: {}) => subscriber.next(data))
+})
+
 chronicleServer.on('connected', () => console.log('connected'))
 chronicleServer.on('disconnected', () => console.log('disconnected'))
 
 chronicleServer.start()
 
-console.log('started')
+console.log('Chronicle consumer started')
--- compose/services/rxdemux/src/index.ts
+++ compose/services/rxdemux/src/index.ts
@@ -1,47 +1,11 @@
-import ligtapi from './light-api'
-import getMassive from './massive-db'
-import { Database } from 'massive'
-import { map } from 'rxjs/operators'
+import { fork$, tx$ } from './chronicle'
 
-const init = async (db: Database) => {
-  ligtapi
-    .pipe(
-      map(({ balances, account }) => {
-        console.log('=================================')
-        console.log(JSON.stringify({ balances, account }))
-        return balances.map(balance => {
-          return {
-            ...balance,
-            account_name: account,
-            decimals: balance.amount.split('.')[1].length,
-          }
-        })
-      }),
-    )
-    .subscribe({
-      async next(data) {
-        try {
-          data.map(async balance => {
-            // it seems massive doesn't support bulk save / upsert anymore ?
-            if ((await db.balances.save(balance)) === null) {
-              await db.balances.insert(balance)
-            }
-          })
-        } catch (error) {
-          console.log(error)
-        }
-      },
-      error(err) {
-        console.error(`something wrong occurred: ${err}`)
-      },
-      complete() {
-        console.log(' ligtapi done')
-      },
-    })
-}
+tx$.subscribe((data: {}) => {
+  console.log('=== tx ===')
+  console.log(data)
+})
 
-// get db instance and initialize
-;(async function start() {
-  const db = await getMassive()
-  init(db)
-})()
+fork$.subscribe((data: {}) => {
+  console.log('=== fork ===')
+  console.log(data)
+})
